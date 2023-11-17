ee80625511f62670b4c8e0a4ee90c6f28aabc41f
Sok Sothy <122692075+Soksothy@users.noreply.github.com>
2023-11-17 16:38:01 +0700

    banner

--- MyWebProject/JavaScript/web.js
+++ MyWebProject/JavaScript/web.js
@@ -14,3 +14,19 @@ function shopNow_1(){
   var t = a * parseInt(qty);
   alert(t);
 }
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 8000); 
+
+}
